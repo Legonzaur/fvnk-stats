9a431294111858a99f83725245371fa2a3c48526
legonzaur <legonzaur@live.fr>
2022-08-10 23:15:40 +0200

    adding dist subtree

---
+++ js/about.f4046cf5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkstats"]=self["webpackChunkstats"]||[]).push([[443],{8369:function(s,t,n){n.r(t),n.d(t,{default:function(){return d}});var a=n(3396);const e={class:"about"},u=(0,a._)("h1",null,"This is an about page",-1),r=[u];function c(s,t,n,u,c,i){return(0,a.wg)(),(0,a.iD)("div",e,r)}var i=n(6520);class l extends i.w3{}var o=n(89);const f=(0,o.Z)(l,[["render",c]]);var d=f}}]);
+//# sourceMappingURL=about.f4046cf5.js.map
